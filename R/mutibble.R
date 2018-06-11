93183a01e855af52e6501db097dec9304053e882
Stuart Lee <lee.s@wehi.edu.au>
2018-06-11 14:49:07 -0700

    first ideas with mutable tibble

---
+++ R/mutibble.R
@@ -0,0 +1,28 @@
+#' A mutable tibble
+#' 
+#'
+#'  
+.mutibble <- function(.data) {
+  # create a data mask with active bindings from the top environment
+  mask <- rlang::as_data_mask(.data, parent = rlang::empty_env())
+  nms <- names(.data)
+  binding_funs <- lapply(nms, binder, .env = mask)
+  names(binding_funs) <- nms
+  for (name in nms) {
+    makeActiveBinding(name, binding_funs[[name]], mask)
+  }
+  mask
+}
+
+
+binder <- function(.env, sym) {
+  function(v) {
+    if (missing(v)) {
+      rlang::env_get(.env$.top_env, sym)
+    } else {
+      sym <- rlang::sym(sym)
+      expr <- rlang::expr(!!sym <- v)
+      rlang::eval_tidy(expr, data = .env)
+    }
+  }
+}
