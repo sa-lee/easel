14fda200fcd6bb5032df7b351430b67f1b117b11
Stuart Lee <lee.s@wehi.edu.au>
2018-08-24 15:12:56 -0700

    observe state... using view.getState()

---
+++ inst/shiny-examples/basic-brush-persistent/app.R
@@ -0,0 +1,59 @@
+# Testing we can use vega signal listeners to return data back to
+# R
+library(shiny)
+library(dplyr)
+
+ui <- fluidPage(
+  tags$head(
+    tags$title("Brush coords to server side"),
+    tags$script(src = "https://cdn.jsdelivr.net/npm/vega@4.2.0"),
+    includeScript("delayed-view.js")
+  ),
+  tags$div(id = "view"),
+  shiny::verbatimTextOutput("cl")
+)
+
+server <- function(input, output, session) {
+  
+  data <- mtcars %>%
+    select(aes_x = hp, aes_y = mpg) 
+  rownames(data) <- NULL
+  
+  update <- reactive({
+    ranges <- c(input$drag_range_x, input$drag_range_y)
+    if (is.null(ranges)) {
+      aes_fill <- rep("steelblue", 32)
+    } else {
+      x <- data$aes_x
+      y <- data$aes_y
+      x <- unname(x)
+      y <- unname(y)
+      xrange <- ranges[c(1,2)]
+      yrange <- ranges[c(3,4)]
+      aes_fill <- dplyr::if_else(
+        dplyr::between(x, xrange[1], xrange[2]) &
+          dplyr::between(y, yrange[2], yrange[1]),
+        "red",
+        "steelblue"
+      )
+    }
+    data[["aes_fill"]] <- aes_fill
+    data
+  })
+  
+  observe({
+    source <- update()
+    hash <- digest::digest(source)
+    session$sendCustomMessage(type = "source",
+                              message = list(data = jsonlite::toJSON(source),
+                                             hash = hash))
+  })
+  
+  observeEvent(input$view_state, { print(input$view_state) })
+  
+  output$cl <- shiny::renderPrint({
+    update()
+  })
+}
+
+shinyApp(ui, server)
---
+++ inst/shiny-examples/basic-brush-persistent/delayed-view.js
@@ -0,0 +1,47 @@
+var view;
+var source;
+var previous_hash = null;
+var hash;
+
+Shiny.addCustomMessageHandler("source",
+  function(message) {
+    source = message.data;
+    hash = message.hash;
+    // initial insert
+    if (previous_hash === null) {
+      view.insert('source', source);
+      previous_hash = hash;
+    }
+    if (previous_hash == hash) {
+      console.log("no need to update");
+    } else {
+      console.log("now would be a good time");
+      view.change('source', vega.changeset().remove(() => {return true}).insert(source));
+      previous_hash = hash;
+    }
+    
+    Shiny.onInputChange("view_state", view.getState().data);
+
+    
+    view.run();
+  }
+);
+
+
+
+vega.loader()
+    .load('https://api.myjson.com/bins/nfgdk')
+    .then(function(data) { render(JSON.parse(data)); });
+
+function render(spec) {
+	view = new vega.View(vega.parse(spec))
+            .renderer('canvas')  // set renderer (canvas or svg)
+            .initialize('#view') // initialize view within parent DOM container
+            .addSignalListener('drag_range_x', function(name, value) {
+              Shiny.onInputChange('drag_range_x', value);
+            })
+            .addSignalListener('drag_range_y',  function(name, value) {
+              Shiny.onInputChange('drag_range_y', value);
+            })
+    }
+
