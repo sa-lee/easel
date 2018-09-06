66befe4ecdb618228a5a1a20c314d2cc639e23ba
Stuart Lee <lee.s@wehi.edu.au>
2018-09-06 11:23:19 -0700

    Ops for reactive logicals...

--- R/reactive-cols.R
+++ R/reactive-cols.R
@@ -16,6 +16,53 @@ as_reactive_logical.logical <- function(x, expr) {
 }
 
 
+#' adapated from Ops.numeric_version and Jim Hester's code in bench repo
+Ops.reactive_lgl <- function (e1, e2) {
+  if (nargs() == 1L) {
+    stop(
+      sprintf("unary '%s' not defined for \"reactive_lgl\" objects", .Generic),
+         call. = FALSE
+      )
+  }
+  
+  boolean <- switch(.Generic,
+                    `==` = TRUE,
+                    `|` = TRUE,
+                    `&` = TRUE,
+                    `!` = TRUE,
+                    `!=` = TRUE,
+                    `<` = TRUE,
+                    `>` = TRUE,
+                    `<=` = TRUE,
+                    `>=` = TRUE,
+                    FALSE)
+  if (!boolean) {
+    stop(sprintf("'%s' not meaningful for \"reactive_lgl\" objects", .Generic),
+         call. = FALSE)
+  }
+  
+  if (inherits(e1, "reactive_lgl")) {
+    e1_expr <- get_reactive_expr(e1)
+  } else {
+    e1_expr <- enquo(e1)
+  }
+  
+  if (inherits(e2, "reactive_lgl")) {
+    e2_expr <- get_reactive_expr(e2)
+  } else {
+    e2_expr <- enquo(e2)
+  }
+
+  ops_expr <- quo({
+    match.fun(!!.Generic)(!!e1_expr, !!e2_expr)
+  })
+  
+  value <- NextMethod(.Generic)
+  as_reactive_logical(value, ops_expr)
+}
+
+
+
 as_reactive_numeric <- function(x, expr) {
   UseMethod("as_reactive_numeric")
 }
